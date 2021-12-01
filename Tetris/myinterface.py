b08050ca2860e6fb44ff7a01e10c921e7b6b00ca
peena5370 <67957824+peena5370@users.noreply.github.com>
2021-12-01 16:12:38 +0800

    Update myinterface.py

--- Tetris/myinterface.py
+++ Tetris/myinterface.py
@@ -1,3 +1,7 @@
+"""
+Source: https://zetcode.com/pyqt6/tetris/
+Author: Jan Bodnar
+"""
 import sys, random
 from PyQt5.QtCore import QBasicTimer, Qt, pyqtSignal
 from PyQt5.QtGui import QColor, QPainter
