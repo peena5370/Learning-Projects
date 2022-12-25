6e914456f2e93696864368432088cbc04a2e2c4c
peena5370 <67957824+peena5370@users.noreply.github.com>
2022-12-25 23:45:55 +0800

    Update student_schema.sql

--- student-management-system/student_schema.sql
+++ student-management-system/student_schema.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE `student_schema`;
 
+-- Create table query for student table
 CREATE TABLE `student`(
     `id` INT NOT NULL AUTO_INCREMENT,
     `studname` VARCHAR(100) NOT NULL,
@@ -9,3 +10,6 @@ CREATE TABLE `student`(
     `nationality` VARCHAR(50) NULL,
     PRIMARY KEY(`id`)
 )ENGINE=INNODB;
+
+-- Insert default value into table
+INSERT INTO `student` VALUES(1, 'Jessica Loh', '2012-02-15', '6RED', 'Chinese', 'Malaysia');
