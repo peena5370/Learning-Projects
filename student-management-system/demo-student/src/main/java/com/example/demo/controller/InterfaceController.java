c3d213d1503b2ec9b4238becf9f49799ef0d582c
Caroline Lee <67957824+peena5370@users.noreply.github.com>
2022-12-27 19:41:06 +0800

    Update InterfaceController.java

--- student-management-system/demo-student/src/main/java/com/example/demo/controller/InterfaceController.java
+++ student-management-system/demo-student/src/main/java/com/example/demo/controller/InterfaceController.java
@@ -3,6 +3,11 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+* InterfaceController which used to loads html contents.
+* @author Caroline Lee
+* Created on 13-05-2022
+**/
 @Controller
 public class InterfaceController {
 	@GetMapping("/student/main")
