e84bf2f42057fccafd85bfbd8f7e37be22cbdd5b
Caroline Lee <67957824+peena5370@users.noreply.github.com>
2022-12-27 19:42:23 +0800

    Update CorsConfig.java

--- student-management-system/demo-student/src/main/java/com/example/demo/utils/CorsConfig.java
+++ student-management-system/demo-student/src/main/java/com/example/demo/utils/CorsConfig.java
@@ -6,20 +6,21 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // CORS policy configuration
-@Configuration//配置类
+@Configuration
 public class CorsConfig {
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")//对所有请求路径
-		          .allowCredentials(true)//允许cookie等凭证
-				  .allowedOriginPatterns("*")//允许所有域名
-				  .allowedMethods("GET", "POST", "DELETE", "PUT","PATCH")//允许所有方法
-				  .maxAge(3600);  
-			}
+			
+		  @Override
+		  public void addCorsMappings(CorsRegistry registry) {
+		    registry.addMapping("/**")
+		    .allowCredentials(true)
+		    .allowedOriginPatterns("*")
+		    .allowedMethods("GET", "POST", "DELETE", "PUT","PATCH")
+		    .maxAge(3600);  
+		  }
 		};
 	}
 }
