944b06e3d6e9f440ebb33f6dbdea3341d7dd2bd0
Caroline Lee <67957824+peena5370@users.noreply.github.com>
2022-12-28 20:25:34 +0800

    Update MainApplication.java

--- file-move/src/main/java/com/file/demo/MainApplication.java
+++ file-move/src/main/java/com/file/demo/MainApplication.java
@@ -32,19 +32,19 @@ public class MainApplication {
 		@Override
 		public void run() {
 			try {
-        // Loads file source and destination properties
+        			// Loads file source and destination properties
 				FileSourceResponse sourceResp = fileUtils.getFileDirectory("./config.properties");
 
 				Path sourcePath = Paths.get(sourceResp.getSourceDir());
 				Path destPath = Paths.get(sourceResp.getDestDir());
 
-        // Create directories if file paths not exists
+        			// Create directories if file paths not exists
 				Files.createDirectories(sourcePath);
 				Files.createDirectories(destPath);
 
 				SAXReader reader = new SAXReader();
 
-        // Scan through all xml files inside the source path
+        			// Scan through all xml files inside the source path
 				Files.walk(sourcePath).forEach(p -> {
 					try {
 						Document document = reader.read(new File(p.toAbsolutePath().toString()));
@@ -57,7 +57,7 @@ public class MainApplication {
 							Path newPath = Paths.get(destPath.toString(), dateArr[0], dateArr[1], dateArr[2]);
 							Files.createDirectories(newPath);
 
-              // Moves file to destination path
+              						// Moves file to destination path
 							Files.move(p.toAbsolutePath(), newPath.resolve(p.getFileName()));
 						} else {
 							System.out.println("not bool");
