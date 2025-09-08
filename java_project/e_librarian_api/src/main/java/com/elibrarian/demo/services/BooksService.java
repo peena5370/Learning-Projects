77906711c19ce72eaf59a46cdd264fd73aa40c87
shengxian.lee <shengxian.lee@archerlogic.com>
2025-09-08 09:33:20 +0800

    [2025090800] #1 Update java elibrarian-api project by adding in learning code for enabling and using multithreading feature, and implement transaction on the code with both functional and declarative way

--- java_project/e_librarian_api/src/main/java/com/elibrarian/demo/services/BooksService.java
+++ java_project/e_librarian_api/src/main/java/com/elibrarian/demo/services/BooksService.java
@@ -2,13 +2,14 @@ package com.elibrarian.demo.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 import com.elibrarian.demo.model.BooksEntity;
 
 public interface BooksService {
     BooksEntity addBook(BooksEntity book);
 
-    List<BooksEntity> viewBooks();
+    CompletableFuture<List<BooksEntity>> viewBooks();
 
     Optional<BooksEntity> viewBookById(Integer book_id);
 
